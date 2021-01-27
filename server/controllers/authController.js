2dcdcda111fc67c10c0790ce82e4c9790c11d0e5
dansilvaUT <dan13silv@gmail.com>
2021-01-27 15:25:18 -0800

    Controllers set up. Logic needed

---
+++ server/controllers/authController.js
@@ -0,0 +1,21 @@
+module.exports = {
+    register: (req, res) => {
+
+    },
+
+    login: (req, res) => {
+
+    },
+
+    logout: (req, res) => {
+
+    },
+
+    getUser: (req, res) => {
+
+    },
+
+    getUsers: (req, res) => {
+
+    }
+}
---
+++ server/controllers/canyonController.js
@@ -0,0 +1,21 @@
+module.exports = {
+    getCanyons: (req, res) => {
+
+    },
+
+    getCanyon: (req, res) => {
+
+    },
+
+    addCanyon: (req, res) => {
+
+    },
+
+    deleteCanyon: (req, res) => {
+
+    },
+
+    editCanyon: (req, res) => {
+
+    }
+}
