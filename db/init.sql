c1f2bdd1ac9b354dc6c1d8b402a5e6a1fce387d7
dansilvaUT <dan13silv@gmail.com>
2021-01-27 14:44:23 -0800

    Created DB tables

---
+++ db/init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS canyon_users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(60) NOT NULL,
+    email VARCHAR(60) NOT NULL,
+    profile_pic TEXT,
+    password VARCHAR(250) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS canyons (
+    canyon_id SERIAL PRIMARY KEY,
+    canyon_name VARCHAR(50) NOT NULL,
+    canyon_description TEXT NOT NULL,
+    canyon_pic TEXT NOT NULL,
+    canyon_rating INTEGER,
+    canyon_city VARCHAR(100),
+    canyon_state VARCHAR(20),
+    canyon_owner INTEGER REFERENCES canyon_users(user_id)
+)
+
+CREATE TABLE IF NOT EXISTS comments (
+    comment_id SERIAL PRIMARY KEY,
+    user_comment TEXT NOT NULL,
+    canyon_id INTEGER  REFERENCES canyons(canyon_id),
+    comment_owner INTEGER REFERENCES canyon_users(user_id)
+)
