432b0b34a50b74390304372bb50c1ce1e4261e76
dansilvaUT <dan13silv@gmail.com>
2021-02-03 10:21:52 -0800

    finished edit function on comments.

--- src/Components/Comments/EditComment/EditComment.js
+++ src/Components/Comments/EditComment/EditComment.js
@@ -6,18 +6,22 @@ import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import '../comment.scss';
+
+
 class EditComment extends Component {
 
     constructor() {
         super();
         this.state = {
-            comment: ''
+            comment: this.props?.comment || ''
         }
     }
 
     componentDidMount() {
-        this.props.getComment(this.props.comment_id);
-        this.setState({ comment: this.props.comment })
+        const { id } = this.props.match.params;
+        const parsedID = parseInt(id);
+        this.props.getComment(parsedID);
+        this.setState({ comment: this.props?.comment })
     }
 
     handleInputChange = value => {
@@ -25,8 +29,16 @@ class EditComment extends Component {
     }
 
     editComment = () => {
-        const { comment_id: id } = this.state.comment;
-        axios.put(`/api/comment/${id}`);
+        const { comment } = this.state;
+        const { id } = this.props.match.params;
+        const parsedID = parseInt(id);
+
+        axios.put(`/api/comment/${parsedID}`, { comment })
+            .then(() => {
+                alert('Comment Updated!');
+                this.props.history.goBack();
+            })
+            .catch(err => console.log(`Client Error: ${err.message}`));
     }
     render() {
         return (
@@ -36,17 +48,19 @@ class EditComment extends Component {
                     name='comment'
                     onChange={(e) => this.handleInputChange(e.target.value)}
                     value={this.state.comment}
-                    
                 />
                 <Button variant="contained" color="primary" onClick={() => this.editComment()}>Update</Button>
             </Container>
+
+
         );
     }
 }
 
 const mapStateToProps = reduxState => {
     return {
-        comment: reduxState.commentReducer.comment.data?.user_comment
+        comment: reduxState.commentReducer.comment.data?.user_comment,
+        commentID: reduxState.commentReducer.comment.data?.comment_id
     }
 }
 export default connect(mapStateToProps, { getComment })(EditComment);
