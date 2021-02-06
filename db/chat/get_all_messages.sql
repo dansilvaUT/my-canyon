00a8e5d3358d2ed743977d7cc895fd2147a6f1e7
dansilvaUT <dan13silv@gmail.com>
2021-02-05 18:17:24 -0800

    finished styling chat part

--- db/chat/get_all_messages.sql
+++ db/chat/get_all_messages.sql
@@ -1,2 +1,7 @@
-SELECT * FROM messages
+-- SELECT * FROM messages
+-- WHERE room_id = ${room_id}
+
+select message_id, message, sender_id, date_sent, username from messages
+
+join canyon_users on canyon_users.user_id = messages.sender_id
 WHERE room_id = ${room_id}
