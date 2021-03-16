19204cca707f6398e9fc2cd0223374e6076d27ca
dansilvaUT <dan13silv@gmail.com>
2021-03-16 16:06:28 -0700

    error handling on login/register page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { getUser } from '../../redux/reducers/userReducer';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
 import './login.scss';
 
 class Login extends Component {
@@ -14,11 +15,12 @@ class Login extends Component {
             email: '',
             password: '',
             verPassword: '',
-            registerView: false
+            registerView: false,
+            message: '',
+            error: false
         }
     }
-    //TODO
-    //CREATE HELPER FUNCTION TO CHECK FOR EMPTY FIELDS
+
     handleInputChange = e => {
         this.setState({ [e.target.name]: e.target.value });
     }
@@ -27,25 +29,53 @@ class Login extends Component {
         this.setState({ registerView: !this.state.registerView });
     }
 
+    toggleError = () => {
+        this.setState({ error: !this.state.error });
+    }
+
+    handleError = message => {
+        this.setState({ message });
+        this.toggleError();
+        setTimeout(() => {
+            this.setState({ message: '' });
+            this.toggleError();
+        }, 1500);
+    }
+
     handleLogin = () => {
         const { username, password } = this.state;
-
+        if (username === '' || password === '') {
+            return this.handleError('All fields must be filled out!');
+        }
         axios.post('/api/auth/login', { username, password })
             .then(res => {
                 this.props.getUser(res.data);
                 this.props.history.push('/profile');
             })
-            .catch(err => console.log(`Error: ${err.message}`));
+            .catch(err => {
+                console.log(`Error: ${err.message}`);
+                this.handleError(err.response.data);
+            });
     }
 
     hanldeSignUp = () => {
-        const { username, email, password } = this.state;
+        const { username, email, password, verPassword } = this.state;
+        if (username === '' || email === '' || password === '') {
+            return this.handleError('All fields must be filled out!');
+        }
+        if (password !== verPassword) {
+            return this.handleError('Passwords do not match!');
+        }
+
         axios.post('/api/auth/register', { username, email, password })
             .then(res => {
                 this.props.getUser(res.data);
                 this.props.history.push('/canyons');
             })
-            .catch(err => console.log(`Error: ${err.message}`));
+            .catch(err => {
+                console.log(`Error: ${err.message}`);
+                this.handleError(err.response.data);
+            });
     }
 
     render() {
@@ -53,6 +83,11 @@ class Login extends Component {
             <section className='main'>
                 <h1 className='heading welcome-heading'>Welcome to MyCanyon!</h1>
                 <section className='login'>
+                    {this.state.error
+                        ? <Alert severity="error">{this.state.message}</Alert>
+                        : null
+                    }
+
                     {this.state.registerView
                         ? (
                             <>
